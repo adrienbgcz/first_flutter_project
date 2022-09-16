ac0a9abe0f434d68f25d5637a11033ac49c4282d
Enzo <enaverlant@gmail.com>
2022-09-16 09:22:58 +0200

    :rocket: Random word + adding favorites

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
+import 'package:first_project/favoritepage.dart';
 import 'package:first_project/randomword.dart';
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
 void main() {
-  runApp(const MyApp());
+  // runApp(const MyApp(saved: []));
+  runApp(const MyNewApp());
 }
 
-class MyApp extends StatelessWidget { // stateless = pas de changements dans l'interface
-  const MyApp({super.key});
+class MyNewApp extends StatefulWidget {
+  const MyNewApp({super.key});
 
+  @override
+  State<MyNewApp> createState() => _MyNewAppState();
+}
+
+class _MyNewAppState extends State<MyNewApp> {
+  List<WordPair> favorites = [];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,9 +26,42 @@ class MyApp extends StatelessWidget { // stateless = pas de changements dans l'i
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const RandomWord(),
-
+        body: RandomWord(
+          saved: favorites,
+          callback: addFavorite,
         ),
-      );
+      ),
+    );
+  }
+
+  void addFavorite({required WordPair word}) {
+    setState(() {
+      if (!favorites.contains(word)) {
+        favorites.add(word);
+      } else {
+        favorites.remove(word);
+      }
+    });
+    print(favorites.length);
   }
 }
+
+// class MyApp extends StatelessWidget { // stateless = pas de changements dans l'interface
+//   const MyApp({super.key, required this.saved});
+//   final List<WordPair> saved;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Welcome to Flutter',
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Welcome to Flutter'),
+//         ),
+//         body: const RandomWord(saved: [],),
+
+//         ),
+//       );
+//   }
+// }
